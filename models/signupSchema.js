09b5d4e292839ad6098b04e65feae2909dc99e04
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-07 14:29:39 +0530

    added schema for signup

---
+++ models/signupSchema.js
@@ -0,0 +1,72 @@
+//mongoose schema
+const mongoose = require("mongoose");
+const uniquid = require("uniquid");
+
+const userSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    default: uniquid(),
+  },
+  name: {
+    type: String,
+    required: [true, "Please enter first name"],
+    validate: [
+      {
+        validator: function () {
+          return this.name.trim().length;
+        },
+        message: "Employee's First name should not be empty",
+      },
+      {
+        validator: function () {
+          const re = /<("[^"]?"|'[^']?'|[^'">])*>/;
+          if (re.test(this.name)) {
+            return false;
+          }
+        },
+        message: "Field content cannot be HTML",
+      },
+    ],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email. "],
+    unique: [true, "Email Id already exist. Try with another Email Id. "],
+    validate: [
+      {
+        validator: function () {
+          return this.email.trim().length;
+        },
+        message: "Employee's Email should not be empty",
+      },
+      {
+        validator: function () {
+          const re = /<("[^"]?"|'[^']?'|[^'">])*>/;
+          if (re.test(this.email)) {
+            return false;
+          }
+        },
+        message: "Field content cannot be HTML",
+      },
+      {
+        validator: function () {
+          const re = /<("[^"]?"|'[^']?'|[^'">])*>/;
+          if (re.test(this.email)) {
+            return false;
+          }
+        },
+        message: "Email id is not valid",
+      },
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter last name"],
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, "Please enter last name"],
+  },
+});
+let User = mongoose.model("User", userSchema);
+module.exports = User;
