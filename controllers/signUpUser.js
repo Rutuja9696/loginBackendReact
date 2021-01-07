4efb77c3b91d5da0c26e88a885ab54bcc34acc9b
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-07 14:30:27 +0530

    added controller to signup new user

---
+++ controllers/signUpUser.js
@@ -0,0 +1,37 @@
+const User = require("../models/signupSchema");
+
+//custom imports
+const AppError = require("../helpers/appErrorClass");
+const sendErrorMessage = require("../helpers/sendError");
+const sendResponse = require("../helpers/sendResponse");
+
+const signupUser = async (req, res, next) => {
+  try {
+    let data = req.body;
+    console.log(data);
+    let newUser = {};
+    newUser.name = data.name.trim();
+    newUser.email = data.email.trim();
+    newUser.password = data.password.trim();
+    newUser.confirmPassword = data.confirmPassword.trim();
+
+    let user = await User.find({ email: newUser.email });
+    if (user.length) {
+      return sendErrorMessage(
+        new AppError(
+          400,
+          "Unsuccessful",
+          "Email already exist try with another Email id"
+        ),
+        req,
+        res
+      );
+    } else {
+      let data = await User.create(newUser);
+      sendResponse(200, "User added Succesfully", data, req, res);
+    }
+  } catch (err) {
+    return sendErrorMessage(new AppError(400, "unsuccessful", err), req, res);
+  }
+};
+module.exports.signupUser = signupUser;
