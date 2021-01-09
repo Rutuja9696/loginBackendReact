a6e395e97eb41ac0a721bec26098a0c512ed8692
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-09 11:27:34 +0530

    added more routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,11 +2,26 @@ const express = require("express");
 //custom inports
 const { signupUser } = require("../controllers/signUpUser");
 const { loginUser } = require("../controllers/loginUser");
-
 //middlewares
+const {
+  checkRequestBody,
+  isEmailValid,
+  checkConfirmPassword,
+  createPasswordHash,
+  isUserRegistered,
+} = require("../middleware/userMiddleware");
+// const protectRoute = require("../middleware/protectRoute");
 const router = express.Router();
-router.route("/signUp").post(signupUser);
-router.route("/login").post(loginUser);
+router
+  .route("/signup")
+  .post(
+    checkRequestBody,
+    isEmailValid,
+    checkConfirmPassword,
+    createPasswordHash,
+    signupUser
+  );
+router.route("/login").post(checkRequestBody, isUserRegistered, loginUser);
 
 //export router
 module.exports = router;
