6ea844dee3502fc37607ef068e85e99af3d6358f
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-09 11:24:09 +0530

    added middleware for validating inputs

---
+++ README.md
@@ -0,0 +1,95 @@
+# User Login/Signup Backend:
+
+User login/signup backend is built using express server, mongoDB is used to store the data that the user will be posting via signup form .This backend application is hosted on Heroku.
+
+## Getting Started
+
+- [Check the frontend app of the User Login/Signup code](https://github.com/Rutuja9696/signup-login-react.git)
+- [Check the User Login/Signup backend code](https://github.com/Rutuja9696/loginBackendReact.git)
+
+## Features
+
+- Signup User and store the data in mongoDb.
+- Login user.
+- Validation for entering valid email.
+- Allows only signedup users to login.
+- Authentication jwt
+  , generates cookies And creating password as hash.
+
+## Additional Features
+
+- hosted on Heroku platform.
+- used mongoDb to store users data.
+
+### Downloading and Running this Project Locally
+
+1. clone the repository
+
+```
+
+https://github.com/Rutuja9696/loginBackendReact.git
+
+```
+
+2. add a `.env` file inside the root folder
+
+```
+
+DATABASE_URL=your_db_url
+DEBUG=true // set true to use local db and false to use atlas
+PORT=your_port_number
+
+```
+
+Sample for local
+
+```
+
+DATABASE_URL=mongodb://127.0.0.1:27017/user
+
+DEBUG=true
+PORT=4000
+
+```
+
+### Supported Routes
+
+```
+
+/user/signup : (method:post) - to signup new user.
+/user/login : (method:post) - to login existing user.
+
+```
+
+Note:
+
+1. send user data in this format:
+
+```
+
+{
+"name":"Sherlock",
+"email":"sherlock@gmail.com",
+"password":"pass123",
+"confirmPassword":"pass123"
+}
+
+```
+
+## Live Demo of this Project
+
+[hosted backend]()
+
+## References
+
+- [mongoose quick start](https://mongoosejs.com/docs/index.html)
+- [mongoDB documentation reference](https://docs.mongodb.com/manual/introduction/)
+- [uniquid to generate unique ids](https://www.npmjs.com/package/uniqid)
+- [npm express](https://www.npmjs.com/package/express)
+- [axios to make api calls](https://www.npmjs.com/package/axios)
+- [CORS module to talk between cross-origin resources](https://www.npmjs.com/package/cors)
+- [middlewares in nodejs](https://medium.com/@selvaganesh93/how-node-js-middleware-works-d8e02a936113)
+
+## Author
+
+- **Rutuja Patil** - _work by_ - [irutujapatil](https://github.com/Rutuja9696)
---
+++ middleware/userMiddleware.js
@@ -0,0 +1,94 @@
+const fs = require("fs");
+const path = require("path");
+const bcrypt = require("bcryptjs");
+const User = require("../models/signupSchema");
+const sendErrorMessage = require("../helpers/sendError");
+const AppError = require("../helpers/appErrorClass");
+// const fileName = path.join(__dirname, "..", "data", "userData.json");
+// const users = JSON.parse(fs.readFileSync(fileName, "utf-8"));
+
+//middleware
+const checkRequestBody = (req, res, next) => {
+  let validationArray = [];
+  switch (req.url) {
+    case "/signup":
+      validationArray = ["email", "password", "confirmPassword"];
+      break;
+    case "/login":
+      validationArray = ["email", "password"];
+      break;
+  }
+  next();
+};
+//to check for valid emailId
+const isEmailValid = (req, res, next) => {
+  next();
+};
+//confirming password
+const checkConfirmPassword = (req, res, next) => {
+  if (req.body.password !== req.body.confirmPassword) {
+    return sendErrorMessage(
+      new AppError(
+        400,
+        "Unsuccessful",
+        "Confirm password & password don't match"
+      ),
+      req,
+      res
+    );
+  }
+  next();
+};
+//check for duplicate email
+// const isEmailUnique = (req, res, next) => {
+//   let findUser = users.find((user) => {
+//     return user.email == req.body.email;
+//   });
+//   if (findUser) {
+//     return res.send("User already registered");
+//   }
+//   next();
+// };
+//create password hash
+const createPasswordHash = async (req, res, next) => {
+  try {
+    let salt = await bcrypt.genSalt(10);
+    req.body.password = await bcrypt.hash(req.body.password, salt);
+    next();
+  } catch (err) {
+    return sendErrorMessage(
+      new AppError(500, "Unsuccessful", "Internal Server Error"),
+      req,
+      res
+    );
+  }
+};
+//to check if user is registered
+const isUserRegistered = (req, res, next) => {
+  console.log(req.body.email);
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      console.log("User", user);
+      // findUser = user;
+      req.currentUser = user;
+      if (!user) {
+        return sendErrorMessage(
+          new AppError(402, "Unsuccessful", "User not Registered"),
+          req,
+          res
+        );
+      }
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+//exporting middlewares
+module.exports.checkRequestBody = checkRequestBody;
+module.exports.isEmailValid = isEmailValid;
+module.exports.checkConfirmPassword = checkConfirmPassword;
+// module.exports.isEmailUnique = isEmailUnique;
+module.exports.createPasswordHash = createPasswordHash;
+module.exports.isUserRegistered = isUserRegistered;
